b7bba7464a867f955789cbbe627f8e2e247c3819
Your Name <nskein19@gmail.com>
2025-08-11 22:57:14 +0545

    Add challenges folder with high_time.ino file

--- challenges/high_time.ino
+++ challenges/high_time.ino
@@ -1,1 +1,30 @@
 // Replace ___ with your code
+const int ledPin = 2;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(115200);
+  Serial.println("Enter the number of seconds to keep the LED ON:");
+}
+
+void loop() {
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    input.trim(); // remove whitespace
+
+    int duration = input.toInt();
+    if (duration > 0) {
+      Serial.print("Turning LED ON for ");
+      Serial.print(duration);
+      Serial.println(" seconds...");
+      
+      //Write your code here
+      ___
+      
+      Serial.println("LED OFF. Enter another number:");
+    } else {
+      Serial.println("Invalid input! Please enter a positive integer:");
+    }
+  }
+}
